81c6992cefd471069e53b90fbaddd0179ddaeb2f
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-05-06 14:03:58 +0530

    feat: add transaction log for BankAccount class

--- section_a_oop/a2_safebank.py
+++ section_a_oop/a2_safebank.py
@@ -6,8 +6,9 @@ class BankAccount:
         self.acc_holder_name = acc_holder_name
         self.__balance = initial_amount
         print(f"Account Holder: {acc_holder_name}, Amount added: {initial_amount}")
-
-        self.transactions = []
+        
+        self.transaction_details = []
+        self.transaction_details.append(f"Account created, Account Holder: {acc_holder_name}, Initial deposit amount: {initial_amount}")
 
     def get_balance(self):
         return self.__balance
@@ -17,6 +18,7 @@ class BankAccount:
             raise ValueError("Amount should not be negative or zero")
         else:
             self.__balance += amount
+            self.transaction_details.append(f"Amount Deposited: {amount}, Balance Amount: {self.__balance}")
             return self.__balance
 
     def withdraw(self, amount):
@@ -27,6 +29,7 @@ class BankAccount:
             raise ValueError(f"Insufficient balance: {self.__balance}, Entered amount: {amount}")
         
         self.__balance -= amount
+        self.transaction_details.append(f"Amount Withdraw: {amount}, Balance Amount: {self.__balance}")
         return self.__balance
     
     def print_statement(self):
@@ -39,12 +42,46 @@ class BankAccount:
 #     print(e)
 
 ba = BankAccount("Aruna", 500)
-print(f"Total balance amount: {ba.get_balance()}")
+# print(f"Total balance amount: {ba.get_balance()}")
 
-print(f"Deposited Amount: {ba.deposit(100)}")
+# print(f"Deposited Amount: {ba.deposit(100)}")
 
 # try:
 #     ba.withdraw(10000)
 #     print(f"Withdraw Amount: {ba.get_balance()}")
 # except ValueError as e:
 #     print(e)
+
+# print(ba.acc_holder_name)
+# ba.acc_holder_name = "siddarth"
+# print(ba.acc_holder_name)
+
+# print(ba.__balance)
+# print(ba.get_balance())
+# ba.__balance = 100
+# print(ba.get_balance())
+# ba.deposit(2500)
+# print(ba.get_balance())
+# print(ba.transaction_details)
+# ba.deposit(2500)
+# print(ba.transaction_details)
+# ba.withdraw(100)
+# print(ba.transaction_details)
+
+# ba = BankAccount("Bala", 100)
+
+# print(ba.transaction_details)
+# ba.deposit(2500)
+# print(ba.transaction_details)
+
+# acc = BankAccount("Arun Kumar", 1000)
+# print(acc.transaction_details)
+
+# acc.deposit(500)
+# print(acc.transaction_details)
+
+# acc.withdraw(300)
+# print(acc.transaction_details)
+
+# acc.withdraw(5000)
+# print(acc.transaction_details)
