3e02f555280da08ec99915bd4d3f9e1c1ba8993e
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-05-06 17:19:12 +0530

    feat: add Driver class with vehicle Composition

--- section_a_oop/a3_quickride.py
+++ section_a_oop/a3_quickride.py
@@ -25,6 +25,16 @@ class Car(Vehicle):
         base= 50
         total_cal = distance_km * rate
         return base + total_cal
+
+class Driver:
+    def __init__(self, name, license_no, vehicle):
+        self.name = name
+        self.license_no = license_no
+        self.vehicle = vehicle
+
+class Ride:
+    def __init__(self):
+        pass
     
 
 # v = Vehicle("TN-37 CW-5100")
@@ -43,3 +53,6 @@ class Car(Vehicle):
 # a = Auto("TN-37 CW-5100")
 # print(a.calculate_fare(40))
 
+d = Driver("Chandru", "TNQWE4123", Auto("TN-23-QW-4312"))
+print(d.vehicle.vehicle_number)
+print(d.vehicle.calculate_fare(10))
